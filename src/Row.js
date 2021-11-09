81c525ce60e7b732a41cb18f2e8a4604acac7138
Ali Hamidizadeh <alihamidizadeh@Alis-MacBook-Pro.local>
2021-11-09 13:33:57 -0800

    movie posters are shown in the DOM

--- src/Row.js
+++ src/Row.js
@@ -1,20 +1,27 @@
 import React, {useState, useEffect} from 'react'
 import axios from "./axios";
 
+const base_url = "https://image.tmdb.org/t/p/w500";
 function Row({title, fetchUrl}) {
-    // const [movies, setmovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         async function fetchData () {
             const request = await axios.get(fetchUrl)
-            console.log(request);
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+    console.log(movies);
     return (
         <div className="Netflix-orginals">
             <h1>{title}</h1>
+            <div className="row_posters">
+              {movies.map((movie) => (
+                <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+              ))}
+            </div>
         </div>
     )
 }
